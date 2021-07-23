637a1a0d18fadc9a4252885c976ea2832c3643d4
retired-guy <seasbs@penguin>
2021-07-23 06:57:49 -0700

    first commit

---
+++ screencontrols.py
@@ -0,0 +1,63 @@
+#screen brightness lib
+#import pigpio
+import os
+from threading import Timer
+
+#gpio = pigpio.pi()
+screenstate = 0
+t = None
+
+def screenon():
+    global screenstate
+    global t
+    global gpio
+
+    if not t is None:
+      try:
+        t.cancel()
+        print("timeout cancelled!")
+        t = None
+      except:
+        pass
+
+    if screenstate == 1:
+      return
+
+    screenstate = 1
+    try:
+#      gpio.set_PWM_dutycycle(19, 50)
+      os.system("vcgencmd display_power 1")
+      os.system("setterm -cursor off")
+      print("screen brightness set")
+    except Exception as e:
+      print(e)
+      pass
+
+def blankscreen():
+    global screenstate
+#    global gpio
+
+    if screenstate == 0:
+        return
+
+    if t is None:
+      return
+
+    screenstate = 0
+
+    try:
+#      gpio.set_PWM_dutycycle(19, 0)
+      os.system("vcgencmd display_power 0")
+      print("screen blanked")
+    except Exception as e:
+      print(e)
+      pass
+
+def screenoff():
+    global t
+
+    if t is None:
+      t = Timer(10, blankscreen)
+      t.start()
+      print("timeout started!")
+
